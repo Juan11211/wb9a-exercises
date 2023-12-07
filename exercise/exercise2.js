4d067e522fd6d96535ef5a0f863512338dced18d
Juan11211 <avilesjuan628@gmail.com>
2023-12-07 15:47:13 -0500

    working on exercises

--- exercise/exercise2.js
+++ exercise/exercise2.js
@@ -11,26 +11,56 @@ class Employee {
         return `${this.firstName} ${this.lastName}`;
     }
 
+    getFullName() {
+        return this.firstName + " " + this.lastName;
+        }
+
     promote(newJobTitle, newPayRate) {
         this.jobTitle = newJobTitle;
         this.payRate = newPayRate;
     }
+
+    // The getIntro() method returns a string
+    getIntro() {
+        let intro =
+        "Hi! I'm " + this.getFullName() + " and I am a " +
+        this.jobTitle;
+        return intro;
+        }
 }
 
-let employee1 = new Employee(1, "Ian", "Auston", "Graphic Artist", 42.50);
+//let employee1 = new Employee(1, "Ian", "Auston", "Graphic Artist", 42.50);
 
-console.log(`Employee ${employee1.fullName} created`);
-console.log(`Job title is ${employee1.jobTitle}`);
-console.log(`Pay rate is $${employee1.payRate}`);
 
-let employee2 = new Employee(2, "Alice", "Smith", "Software Developer", 60.00);
+// let employee1 = new Employee(
+//     1, "Ian", "Auston", "Graphic Artist", 42.50);
+//     console.log(`Employee ${employee1.firstName} created`); 
 
-console.log(`Employee ${employee2.fullName} created`);
-console.log(`Job title is ${employee2.jobTitle}`);
-console.log(`Pay rate is $${employee2.payRate}`);
+// console.log(`Employee ${employee1.getFullName} created`);
+// console.log(`Job title is ${employee1.jobTitle}`);
+// console.log(`Pay rate is $${employee1.payRate}`);
 
-employee2.promote("Senior Software Developer", 75.00);
+// let employee2 = new Employee(2, "Alice", "Smith", "Software Developer", 60.00);
 
-console.log(`Employee ${employee2.fullName} has been promoted`);
-console.log(`New job title is ${employee2.jobTitle}`);
-console.log(`New pay rate is $${employee2.payRate}`);
+// console.log(`Employee ${employee2.fullName} created`);
+// console.log(`Job title is ${employee2.jobTitle}`);
+// console.log(`Pay rate is $${employee2.payRate}`);
+
+// employee2.promote("Senior Software Developer", 75.00);
+
+// console.log(`Employee ${employee2.fullName} has been promoted`);
+// console.log(`New job title is ${employee2.jobTitle}`);
+// console.log(`New pay rate is $${employee2.payRate}`);
+
+// let intro = employee1.getIntro();
+// console.log(intro);
+
+let employee2 = new Employee(
+    1, "Juan", "Aviles", "SWE", 42.50);
+    console.log(`Employee ${employee2.fullName} created`);
+    console.log(`Job title is ${employee2.jobTitle}`);
+    console.log(`Pay rate is $${employee2.payRate}`);
+
+    
+let intro = employee2.getIntro();
+console.log(intro);
